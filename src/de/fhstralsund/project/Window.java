1b5c4b2fa4fbccef1838444e054a1602c09a03fa
deB4SH <github@b4sh.de>
2014-09-29 11:55:13 +0200

    basic project setup

--- src/de/fhstralsund/project/Window.java
+++ src/de/fhstralsund/project/Window.java
@@ -2,53 +2,44 @@ package de.fhstralsund.project;
 
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 public class Window {
 
-    private int windowWidth = 800, windowHeight = 600;
-
-    public Window() {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.start();
     }
 
-    public Window(int windowWidth, int windowHeight) {
-        this.windowWidth = windowWidth;
-        this.windowHeight = windowHeight;
-    }
-
-    public void start(){
-        try{
-            Display.setDisplayMode(new DisplayMode(windowWidth, windowHeight));
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800,600));
             Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
             System.exit(0);
         }
 
-        //init OpenGL
+        // init OpenGL
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
-        GL11.glOrtho(0,windowWidth,0,windowHeight,1,-1);
+        GL11.glOrtho(0,800,0,600,1,-1);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-        while(!Display.isCloseRequested()){
-            //clear Scene w/ cornflowerblue
+        while (!Display.isCloseRequested()) {
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            GL11.glColor3f(100f, 149f, 237f);
-
+            GL11.glColor3f(0.5f,0.5f,1.0f);
             GL11.glBegin(GL11.GL_QUADS);
-
-            //frontface
-            GL11.glVertex3f(Mouse.getX()-50,Mouse.getY()-50,0);
-            GL11.glVertex3f(Mouse.getX()+50,Mouse.getY()-50,0);
-            GL11.glVertex3f(Mouse.getX()+50,Mouse.getY()+50,0);
-            GL11.glVertex3f(Mouse.getX()-50,Mouse.getY()+50,0);
-
+                GL11.glVertex2f(100,100);
+                GL11.glVertex2f(100+200,100);
+                GL11.glVertex2f(100+200,100+200);
+                GL11.glVertex2f(100,100+200);
             GL11.glEnd();
-
+            Display.update();
         }
+
+        Display.destroy();
     }
 }
