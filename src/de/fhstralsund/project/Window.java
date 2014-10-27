25b896d689b1ea16deb89a70e5d6a07b92d142d3
N0V0S <Novos90@googlemail.com>
2014-10-27 11:36:10 +0100

    Created Map

--- src/de/fhstralsund/project/Window.java
+++ src/de/fhstralsund/project/Window.java
@@ -2,16 +2,21 @@ package de.fhstralsund.project;
 
 
 import de.b4sh.core.resource.ResourceStorage;
+import de.fhstralsund.project.Map.Map;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
+import java.io.IOException;
+
 public class Window {
+
+    private Map map;
 
     public static void main(String[] args) {
 
-        ResourceStorage resourceStorage = new ResourceStorage("res");
+        ResourceStorage resourceStorage = new ResourceStorage();
 
         Window window = new Window();
         window.start();
@@ -32,16 +37,10 @@ public class Window {
         GL11.glOrtho(0,800,0,600,1,-1);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
+        map = new Map(200);
+
         while (!Display.isCloseRequested()) {
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            GL11.glColor3f(0.5f,0.5f,1.0f);
-            GL11.glBegin(GL11.GL_QUADS);
-                GL11.glVertex2f(100,100);
-                GL11.glVertex2f(100+200,100);
-                GL11.glVertex2f(100+200,100+200);
-                GL11.glVertex2f(100,100+200);
-            GL11.glEnd();
-            Display.update();
+            map.Render();
         }
 
         Display.destroy();
