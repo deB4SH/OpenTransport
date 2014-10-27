7832ca1427b7042a1eaed4b637db9b1efbb6b90c
N0V0S <Novos90@googlemail.com>
2014-10-27 11:46:48 +0100

    added map

---
+++ src/de/fhstralsund/project/Entity/IRenderable.java
@@ -0,0 +1,9 @@
+package de.fhstralsund.project.Entity;
+
+/**
+ * Created by Florian on 27.10.2014.
+ */
+public interface IRenderable {
+
+    public void Render();
+}
--- src/de/fhstralsund/project/Window.java
+++ src/de/fhstralsund/project/Window.java
@@ -1,30 +1,19 @@
 package de.fhstralsund.project;
 
 
-import de.b4sh.core.resource.ResourceStorage;
-<<<<<<< HEAD
+
 import de.fhstralsund.project.Map.Map;
-=======
 import de.fhstralsund.project.resource.ResourceLoader;
->>>>>>> 2ff6c0f40e5f2f2897814f2e2daf5439e0e8f213
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
-
-import java.io.IOException;
 
 public class Window {
 
     private Map map;
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-        ResourceStorage resourceStorage = new ResourceStorage();
-
-=======
->>>>>>> 2ff6c0f40e5f2f2897814f2e2daf5439e0e8f213
         Window window = new Window();
         window.start();
     }
