def49de23513be686b1a37241bacc6656305a855
deB4SH <github-mail@b4sh.de>
2014-10-27 12:10:58 +0100

    CHANGE: Window.java
    rewrote all setup things

--- src/de/fhstralsund/project/Window.java
+++ src/de/fhstralsund/project/Window.java
@@ -18,32 +18,50 @@ public class Window {
     }
 
     public void start() {
-        try {
-            this.setup();
+        this.setup();
+        this.setupGL(800,600);
 
-            Display.setDisplayMode(new DisplayMode(800,600));
+        while(true){
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+            this.render();
+            Display.update();
+            Display.sync(100);
+
+            if(Display.isCloseRequested()){
+                Display.destroy();
+                System.exit(0);
+            }
+        }
+    }
+
+    private void render() {
+        map.Render();
+    }
+
+    private void setupGL(int width,int height){
+        try{
+            Display.setDisplayMode(new DisplayMode(width,height));
             Display.create();
+            Display.setVSyncEnabled(true);
         } catch (LWJGLException e) {
             e.printStackTrace();
             System.exit(0);
         }
 
-        // init OpenGL
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0,800,0,600,1,-1);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA,GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+        GL11.glViewport(0,0,width,height);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-        map = new Map(200);
-
-        while (!Display.isCloseRequested()) {
-            map.Render();
-        }
-
-        Display.destroy();
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0,width,height,0,1,-1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
     }
 
     private void setup(){
-       rl  = new ResourceLoader();
+        rl  = new ResourceLoader();
+        map = new Map(200);
     }
 }
