e6770525d0df8a8a587aa8d1b346855f49f0acb4
deB4SH <github@b4sh.de>
2014-10-30 08:32:44 +0100

    BASIC: configclass

--- src/de/fhstralsund/project/core/io/Configreader.java
+++ src/de/fhstralsund/project/core/io/Configreader.java
@@ -1,6 +1,28 @@
 package de.fhstralsund.project.core.io;
 
-import org.
+import org.json.simple.JSONObject;
+
+import java.util.Collection;
+import java.util.HashMap;
+
 
 public class Configreader {
+
+    public Configreader() {
+    }
+
+    public HashMap<String,String> loadConfig(String dir, String filename){
+
+        return new HashMap<String, String>();
+    }
+
+    public void saveConfig(HashMap<String,String> config,String dir, String filename){
+
+        Collection<String> e = config.values();
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("derp","hep");
+
+
+    }
 }
