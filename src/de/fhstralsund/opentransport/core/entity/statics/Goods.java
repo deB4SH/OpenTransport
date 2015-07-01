d79f59f6a49af3ce3aacd26d5549a54fa819a3c1
deB4SH <github@b4sh.de>
2015-07-01 14:48:29 +0200

    NEW: Storage-System for entites dynamic storage system that can hold every item in the game

---
+++ src/de/fhstralsund/opentransport/core/entity/statics/Goods.java
@@ -0,0 +1,7 @@
+package de.fhstralsund.opentransport.core.entity.statics;
+
+/**
+ * Created by b4sh on 24.06.2015.
+ */
+public class Goods {
+}
---
+++ src/de/fhstralsund/opentransport/core/entity/type/Storage.java
@@ -0,0 +1,7 @@
+package de.fhstralsund.opentransport.core.entity.type;
+
+/**
+ * Created by b4sh on 24.06.2015.
+ */
+public class Storage {
+}
